07e0c6f3e47475500605692cab15f736c05c7ea7
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-04 11:30:09 +0300

    Add prints

--- commands/printsForPrinting.js
+++ commands/printsForPrinting.js
@@ -1,11 +1,54 @@
 module.exports = {
-  name: 'website',
-  description: 'this is sends website link, command.',
+  name: 'randomprint',
+  description: 'This commands random prints.',
   execute(message, args) {
-    const pritns = [
-      {name: 'Test', link: 'https://www.thingiverse.com/thing:4965226'}
+    const prints = [
+      {
+        name: 'Half Mask Skull',
+        url: 'https://www.thingiverse.com/thing:4965226'
+      },
+      {
+        name: 'Ornamental Bulbasaur',
+        url: 'https://www.thingiverse.com/thing:4973828'
+      },
+      {name: 'Baby Groot', url: 'https://www.thingiverse.com/thing:2014307'},
+      {
+        name: 'Cute Mini Octopus',
+        url: 'https://www.thingiverse.com/thing:3495390'
+      },
+      {
+        name: 'Fillenium Falcon',
+        url: 'https://www.thingiverse.com/thing:919475'
+      },
+      {
+        name: 'Customazible Sanding Stick',
+        url: 'https://www.thingiverse.com/thing:2404850'
+      },
+      ,
+      {
+        name: 'Flexi Rex',
+        url: 'https://www.thingiverse.com/thing:2738211'
+      },
+      {
+        name: 'Baby Yoda',
+        url: 'https://www.thingiverse.com/thing:4038181'
+      },
+      {
+        name: 'Jet Engine',
+        url: 'https://www.thingiverse.com/thing:1327093'
+      },
+      {
+        name: 'Eiffel Tower',
+        url: 'https://www.thingiverse.com/thing:912478'
+      },
+      {
+        name: 'Spiral Vase',
+        url: 'https://www.thingiverse.com/thing:570288'
+      }
     ]
 
-    message.channel.send('https://zaxe.com')
+    let randomizer = Math.floor(Math.random() * prints.length)
+    let randomPrint = prints[randomizer]
+    message.channel.send(`**${randomPrint.name}**\n${randomPrint.url}`)
   }
 }
