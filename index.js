6de15949ae4c52b61716bedd0a0d283aaf9f08f9
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-04 22:47:49 +0300

    Rename bot.js to index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const fs = require('fs')
+require('dotenv').config()
+client.commands = new Discord.Collection()
+
+const commandFiles = fs
+  .readdirSync('./commands/')
+  .filter((file) => file.endsWith('.js'))
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+
+  client.commands.set(command.name, command)
+}
+
+var prefix = process.env.PREFIX
+
+client.on('ready', () => {
+  console.log(`XYZ Calibrated! => ${client.user.tag}!`)
+  client.user.setActivity({
+    name: 'Zhelp | Zrandomprint',
+    type: 'PLAYING',
+    status: 'idle'
+  })
+})
+
+client.on('message', (message) => {
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
+  console.log(command)
+
+  switch (command) {
+    case 'website':
+      client.commands.get('website').execute(message, args)
+      break
+    case 'ban':
+      client.commands.get('ban').execute(message, args)
+      break
+    case 'help':
+      client.commands.get('help').execute(message, args)
+      break
+    case 'deletemsg':
+      client.commands.get('deletemsg').execute(message, args)
+      break
+    case 'randomprint':
+      client.commands.get('randomprint').execute(message, args)
+      break
+    case 'coding':
+      client.commands.get('coding').execute(message, args)
+      break
+    case 'updatelog':
+      client.commands.get('updatelog').execute(message, args)
+      break
+  }
+})
+
+client.login(process.env.TOKEN)
