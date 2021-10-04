68596f90c82e3b10b8e4e5e0c2ad0c9667c47221
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-04 15:29:53 +0300

    Create coding.js

---
+++ commands/coding.js
@@ -0,0 +1,19 @@
+const fetch = require('cross-fetch')
+
+module.exports = {
+  name: 'coding',
+  description: 'this code sends coding gifs.',
+
+  execute(message, args) {
+    fetch(
+      'https://api.giphy.com/v1/gifs/search?api_key=hGRD0U2gKuXmSUkp4iTGwjx3VrRv4tWm&q=coding'
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let randomizer = parseInt(Math.floor(Math.random() * 45))
+        console.log(randomizer)
+        message.channel.send(data.data[randomizer].url)
+      })
+  }
+}
+//https://api.giphy.com/v1/gifs/random?api_key=hGRD0U2gKuXmSUkp4iTGwjx3VrRv4tWm&tag=code&rating=pg-13 GIPHY RANDOM
