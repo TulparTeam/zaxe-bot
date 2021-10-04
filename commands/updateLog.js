2929218fb8696060e7bbc1e27a621ffa347fbc98
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-04 17:46:31 +0300

    Create updatelog command

--- commands/updateLog.js
+++ commands/updateLog.js
@@ -4,7 +4,7 @@ module.exports = {
   name: 'updatelog',
   description: 'This commands send update log to #update-logs channel.',
   execute(message, args) {
-    if (message.member.roles.has('893439201687764992')) {
+    if (message.member.roles.cache.has('893439201687764992')) {
       message.channel.send('True!')
     } else {
       message.channel.send('False!')
