c6567da454929f3b80d945db7ae96dc597d4e277
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-04 17:39:07 +0300

    Create updatelog command

---
+++ commands/updateLog.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+
+module.exports = {
+  name: 'updatelog',
+  description: 'This commands send update log to #update-logs channel.',
+  execute(message, args) {
+    if (message.member.roles.has('893439201687764992')) {
+      message.channel.send('True')
+    } else {
+      message.channel.send('False')
+    }
+  }
+}
