f855600b99010c96ae9aa3bbd7917f829d8a00de
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-05 11:39:22 +0300

    Change desc

--- commands/ban.js
+++ commands/ban.js
@@ -1,13 +1,13 @@
 module.exports = {
-    name: 'ban',
-    description: 'this is bans a user from server, command.',
-    execute(message, args) {
-        if (message.member.roles.cache.has('801094458594557962')) {
-            message.channel.send('Banlama Başarılı.');
-        }
-        else {
-            message.reply('Görünüşe göre bu komut için gerekli yetkiye sahip değilsin, bu komudu kullanamazsın.');
-        }
-
+  name: 'ban',
+  description: 'This commands is for banning a member.',
+  execute(message, args) {
+    if (message.member.roles.cache.has('801094458594557962')) {
+      message.channel.send('Banlama Başarılı.')
+    } else {
+      message.reply(
+        'Görünüşe göre bu komut için gerekli yetkiye sahip değilsin, bu komudu kullanamazsın.'
+      )
     }
+  }
 }
