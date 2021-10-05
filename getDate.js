ee1b296ffda957af198623cf253b6bcc13e1506d
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-05 15:50:35 +0300

    Change month convert settings

--- getDate.js
+++ getDate.js
@@ -1,11 +1,11 @@
 const sendDate = () => {
   const dateConst = new Date()
-  let nowDay = Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(
-    dateConst.getDay()
-  )
-  let nowMonth = Intl.DateTimeFormat('en-US', {month: 'long'}).format(
-    dateConst.getMonth()
-  )
+  console.log(dateConst)
+  let nowDayNC = parseInt(dateConst.getDay()).toString()
+  let nowMonthNC = parseInt(dateConst.getMonth()).toString()
+  console.log(nowDayNC, nowMonthNC)
+  let nowDay = Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(dateConst)
+  let nowMonth = Intl.DateTimeFormat('en-US', {month: 'long'}).format(dateConst)
   let nowHours = dateConst.getHours()
   let nowMinutes = dateConst.getMinutes()
 
