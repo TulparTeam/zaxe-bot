6447bc917f701c549298f3f11f9c3c74ee5d4644
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-05 15:57:30 +0300

    Remove console logs

--- getDate.js
+++ getDate.js
@@ -4,8 +4,8 @@ const sendDate = () => {
   let nowMonthNC = parseInt(dateConst.getMonth()).toString()
   let nowDay = Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(dateConst)
   let nowMonth = Intl.DateTimeFormat('en-US', {month: 'long'}).format(dateConst)
-  let nowHours = dateConst.getHours()
-  let nowMinutes = dateConst.getMinutes()
+  let nowHours = dateConst.getUTCHours()
+  let nowMinutes = dateConst.getUTCMinutes()
 
   return `[ DATE ] :  ${nowHours}:${nowMinutes}  |  ${nowDay} / ${nowMonth}`
 }
