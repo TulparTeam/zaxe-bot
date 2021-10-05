04329ebc69f6e9a1df4a9eab63d1d2ec654b9f9d
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-05 15:52:42 +0300

    Remove console logs

--- getDate.js
+++ getDate.js
@@ -1,9 +1,7 @@
 const sendDate = () => {
   const dateConst = new Date()
-  console.log(dateConst)
   let nowDayNC = parseInt(dateConst.getDay()).toString()
   let nowMonthNC = parseInt(dateConst.getMonth()).toString()
-  console.log(nowDayNC, nowMonthNC)
   let nowDay = Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(dateConst)
   let nowMonth = Intl.DateTimeFormat('en-US', {month: 'long'}).format(dateConst)
   let nowHours = dateConst.getHours()
